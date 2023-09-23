f01dee5deda406f0759bc7dc4dc72933bd67f7bd
luckywings <137508800+luckywings@users.noreply.github.com>
2023-09-23 17:26:09 +0530

    Add files via upload

---
+++ prgm12.py
@@ -0,0 +1,36 @@
+row=5
+for i in range(row,0,-1):
+    print("*", end="")
+print()
+ro=2
+for i in range(ro,0,-1):
+    print("*", end="")
+print()
+r=3
+for i in range(r,0,-1):
+    print("*", end="")
+print()
+v=4
+for i in range(v,0,-1):
+    print("*", end="")
+print()
+o=1
+for i in range(o,0,-1):
+    print("*", end="")
+print()
+r=3
+for i in range(r,0,-1):
+    print("*", end="")
+print()
+v=4
+for i in range(v,0,-1):
+    print("*", end="")
+print()
+ro=2
+for i in range(ro,0,-1):
+    print("*", end="")
+print()
+r=3
+for i in range(r,0,-1):
+    print("*", end="")
+print()
